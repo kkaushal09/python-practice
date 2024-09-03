a0ddcf1f6dbc7c387c513700fb7481ea5468b50a
kkaushal09 <43533433+kkaushal09@users.noreply.github.com>
2024-09-03 23:45:21 +0530

    Update list-operations.py

--- list-operations.py
+++ list-operations.py
@@ -1,57 +1,59 @@
 # Running code https://onecompiler.com/python/42r4nteke
-
 import re
 
 lst = ["city",123,123,123,0,0,0,6,6,6,98,97,97,97,"bangalore"]
 
 pattern = r'^([a-zA-Z])+$'
 
-def removestr(lst):
-  print(lst)
-  lst1 = []
-  for i in lst:
-    print(i)
-    istr = str(i)
-  #  pattern = re.compile("^([a-zA-Z])+$")
-  # pattern.match(i)
-  # st = re.i(.*[a-zA-z])
+def isstr(str):
+  if re.match(pattern, str):
+    return True
+  return False
   
-    if not re.match(pattern, istr):
-      # print(i)
-      lst1.append(i)
-    # else:
-    # print("Not string")
-      # lst1.append(i)
-      print(lst1)
-    return lst1
+def removestr(lst):
+  lst_num = []
+  for i in lst:
+    str_i = str(i)
+    check_str = isstr(str_i)
+    if not check_str:
+      lst_num.append(i)
+  return lst_num
 
+def lst_sort(lst):
+  lst_len = len(lst)
+  for i in range(0, lst_len):
+    # print(f"I:{i}")
+    for j in range(i+1, lst_len):
+      # print(f"J:{j}")
+      # print(lst[i], lst[j])
+      if lst[i] > lst[j]:
+        print(f"Swapping {lst[i]} and {lst[j]}")
+        # temp = lst[i]
+        # lst[i] = lst[j]
+        # lst[j] = temp
+        lst[i], lst[j] = lst[j],lst[i]
+  return lst
+  
+def n_element(lst,*args):
+  len_lst = len(lst)
+  # print(args)
+  # print(len_lst)
+  for arg in args:
+    if arg >= 0:
+      print(f"Element at pos {arg} is {lst[arg]}")
+    if arg < 0:
+      x = abs(arg)
+      print(f"Element at pos {arg} is {lst[len_lst-x]}")      
+
+  
 lst_new = removestr(lst)
-    
-print(lst_new)
-# print(lst1)
-# s=set(lst1)
-# print(s)
-#for sort    
-srt = []
-for i in range(0,len(lst1)):
-  x = lst1[i]
-  y = lst1[i+1]
-  print(x,y)
-  if x > y:
-    srt[i] = y
-  else:
-    srt[i] = x
-    
-#2nd largest and 2nd smallest number
+print(f"List without string: {lst_new}")
 
-# for i in lst1:
-#   lar = lst1[i]
-#   sec_lar = lst1[i] 
-#   if lst1[i+1] > lst1[i]:
-#     lar = lst1[i+1]
-    
-# second largest number
-lst_len = len(lst1)
-sec_lar = lst1[lst_len-1]
-sec_small = lst1[1]
-    
+lst_new = set(lst_new)
+lst_new = list(lst_new)
+# print(f"Sorted list:{lst_new}")
+sort_lst = lst_sort(lst_new)
+print(f"Sorted list:{sort_lst}")
+
+
+x = n_element(sort_lst,1,3,-3)
